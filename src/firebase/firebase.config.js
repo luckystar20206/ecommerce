e5dfa87c31189f9a224bd4f206ff6b12c9c0679f
luckystar20206 <luckystar20206@gmail.com>
2024-01-17 16:55:41 +0600

    solve this problem

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -17,23 +17,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
-=======
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export default app;
->>>>>>> 9857ca1ffd1f3c1f564ad706a82130ca33e20606
